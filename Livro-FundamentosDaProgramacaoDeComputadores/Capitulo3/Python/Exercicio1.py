2ce314fa540013b2355aa0947c1311f27fed9d56
AnaCanela <anacanellag3@gmail.com>
2024-10-25 17:18:20 -0300

    Atualizacao

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/Python/Exercicio1.py
@@ -0,0 +1,11 @@
+#1. Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
+a = input('Digite o numero 1: ')
+b = input('Digite o numero 2: ')
+c = input('Digite o numero 3: ')
+d = input('Digite o numero 4: ')
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+soma = a + b + c + d
+print('A soma dos numeros eh', soma)
