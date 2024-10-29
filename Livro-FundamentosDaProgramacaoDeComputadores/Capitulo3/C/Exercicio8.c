7e3448cd0f3ad7105354266cfb4fd21022dcc235
AnaCanela <anacanellag3@gmail.com>
2024-10-29 16:19:43 -0300

    Atualização

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/C/Exercicio8.c
@@ -0,0 +1,23 @@
+//Faça um programa que receba o valor de um depósito e o valor da taxa de juros, calcule e mostre o valor do rendimento e o valor total depois do rendimento.
+
+#include <stdio.h>
+
+int main()
+{
+    float deposito, taxa, rendimento = 0, total = 0;
+
+    printf("Digite o valor do deposito: ");
+        scanf("%f", &deposito);
+    
+    printf("Digite a taxa de juros: ");
+        scanf("%f", &taxa);
+
+    taxa = taxa / 100;
+    rendimento = deposito * taxa;
+    total = deposito + rendimento;
+
+    printf("Rendimento: R$ %.2f", rendimento);
+    printf("\nValor total: %.2f", total); 
+
+    return 0;
+}
---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/C/Exercicio9.c
@@ -0,0 +1,20 @@
+//9. Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: Área = (base * altura)/2.
+
+#include <stdio.h>
+
+int main()
+{
+    float base, altura, area = 0;
+    
+    printf("Digite a base: ");
+        scanf("%f", &base);
+    
+    printf("Digite a altura: ");
+        scanf("%f", &altura);
+    
+    area = (base * altura) / 2;
+
+    printf("Area: %.1f", area);
+
+    return 0;
+}
