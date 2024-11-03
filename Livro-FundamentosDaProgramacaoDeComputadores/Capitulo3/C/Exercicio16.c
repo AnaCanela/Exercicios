39549e4ecbd44293bc5d9134ff3aa48d1fc01b13
AnaCanela <anacanellag3@gmail.com>
2024-11-03 15:13:56 -0300

    Create Exercicio16.c

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/C/Exercicio16.c
@@ -0,0 +1,19 @@
+/*16. Faça um programa que receba o número de horas trabalhadas e o valor do salário mínimo, calcule e mostre o salário a receber, seguindo estas regras:
+    a) a hora trabalhada vale a metade do salário mínimo.
+    b) o salário bruto equivale ao número de horas trabalhadas multiplicado pelo valor da hora trabalhada.
+    c) o imposto equivale a 3% do salário bruto.
+    d) o salário a receber equivale ao salário bruto menos o imposto.*/
+
+#include <stdio.h>
+
+int main()
+{
+    float salMin, horasTrab;
+
+    printf("Digite o salario minimo: ");
+        scanf("%f", &salMin);
+    printf("Digite as horas trabalhadas: ");
+        scanf("%f", &horasTrab);
+    
+    
+}
