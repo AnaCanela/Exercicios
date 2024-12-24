ec4b2545e5e5fc65b6c92cc62d51612dc10ed3db
AnaCanela <anacanellag3@gmail.com>
2024-12-24 17:23:29 -0300

    att

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/C/Exercicio25.c
@@ -0,0 +1,21 @@
+/*25. Faça um programa que receba o custo de um espetáculo teatral e o preço do convite desse espetáculo.
+Esse programa deverá calcular e mostrar a quantidade de convites que devem ser vendidos para que,
+pelo menos, o custo do espetáculo seja alcançado.*/
+
+#include <stdio.h>
+
+int main()
+{
+    float ingresso, custo, quantidade = 0;
+
+    printf("Digite o valor do custo: ");
+    scanf("%f", &custo);
+    printf("Digite o valor do ingresso: ");
+    scanf("%f", &ingresso);
+
+    quantidade = custo / ingresso;
+
+    printf("A Quantidade minima de ingressos vendidas deve ser de %.0f.\n", quantidade);
+
+    return 0;
+}
