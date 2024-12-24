b66a8be49f1e465d2a557c287d0ef839c25baf61
AnaCanela <anacanellag3@gmail.com>
2024-12-24 17:17:42 -0300

    att

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/C/Exercicio24.c
@@ -0,0 +1,23 @@
+/*24. Faça um programa que receba uma hora formada por hora e minutos (um número real), calcule e
+mostre a hora digitada apenas em minutos. Lembre-se de que:
+■■ para quatro e meia, deve-se digitar 4.30;
+■■ os minutos vão de 0 a 59.*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float hora, h = 0, m = 0, conversao = 0;
+
+    printf("Digite a hora: "); 
+    scanf("%f", &hora);
+
+    h = floor(hora);
+    m = hora - h;
+    conversao = (h * 60) + (m * 100);
+
+    printf("A hora convertida é igual a %.0f minutos.", conversao);
+
+    return 0;
+}
