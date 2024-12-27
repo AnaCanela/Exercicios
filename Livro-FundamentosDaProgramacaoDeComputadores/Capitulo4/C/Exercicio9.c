b969394e386760fa9b4825fcf26225e62400d739
AnaCanela <anacanellag3@gmail.com>
2024-12-27 19:21:14 -0300

    att

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo4/C/Exercicio9.c
@@ -0,0 +1,9 @@
+/*9. Faça um programa que mostre a data e a hora do sistema nos seguintes formatos: DD/MM/AAAA –
+mês por extenso e hora:minuto.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int dia, mes, ano, hora, minuto
+}
