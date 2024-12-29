6fd11e6abc0ad90f56f5297a34032f8ac24d6297
AnaCanela <anacanellag3@gmail.com>
2024-12-28 21:27:39 -0300

    att

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/Python/Exercicio2.py
@@ -0,0 +1,9 @@
+#2. Faça um programa que receba três notas, calcule e mostre a média aritmética.
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+mediaAritmetica = (nota1 + nota2 + nota3) / 3
+
+print("\nA média aritmética das três notas é ", mediaAritmetica)
