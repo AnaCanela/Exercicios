21b0cb40bd6f5c494f3bea88d68bb5859c05025b
AnaCanela <anacanellag3@gmail.com>
2025-01-04 21:55:08 -0300

    att

--- Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/Java/Exercicio25.java
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/Java/Exercicio25.java
@@ -1,3 +1,21 @@
+/*25. Faça um programa que receba o custo de um espetáculo teatral e o preço do convite desse espetáculo. Esse programa deverá calcular e mostrar a quantidade de convites que devem ser vendidos para que, pelo menos, o custo do espetáculo seja alcançado.*/
+
+import java.util.Scanner;
+
 public class Exercicio25 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite os custos do espetáculo: ");
+        double custo = scan.nextDouble();
+        System.out.print("Digite o preço do ingresso: ");
+        double ingresso = scan.nextDouble();
+
+        double quantidade = custo / ingresso;
+
+        System.out.printf("\nA Quantidade mínima de ingressos vendidas deve ser de %.0f.\n", quantidade);
+
+        scan.close();
+    }
     
 }
