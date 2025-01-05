885e41d4008aea440f80f0bced012192fe05fc41
AnaCanela <anacanellag3@gmail.com>
2025-01-04 23:37:50 -0300

    att

--- Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/C/Exercicio22.c
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/C/Exercicio22.c
@@ -1,5 +1,4 @@
-/*22. Sabe-se que o quilowatt de energia custa um quinto do salário mínimo. Faça um programa que rece-
-ba o valor do salário mínimo e a quantidade de quilowatts consumida por uma residência. Calcule e
+/*22. Sabe-se que o quilowatt de energia custa um quinto do salário mínimo. Faça um programa que receba o valor do salário mínimo e a quantidade de quilowatts consumida por uma residência. Calcule e
 mostre:
 a) o valor de cada quilowatt;
 b) o valor a ser pago por essa residência;
--- Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/Java/Exercicio22.java
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/Java/Exercicio22.java
@@ -1,3 +1,31 @@
+/*22. Sabe-se que o quilowatt de energia custa um quinto do salário mínimo. Faça um programa que receba o valor do salário mínimo e a quantidade de quilowatts consumida por uma residência. Calcule e
+mostre:
+a) o valor de cada quilowatt;
+b) o valor a ser pago por essa residência;
+c) o valor a ser pago com desconto de 15%.*/
+
+import java.util.Scanner;
+
 public class Exercicio22 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o salário mínimo: ");
+        double salario = scan.nextDouble();
+
+        System.out.print("Digite a quantidade de quilowatts consumida: ");
+        double consumido = scan.nextDouble();
+
+        double valorKW = salario / 5;
+        double valorReais = valorKW / consumido;
+        double desconto = valorReais * 15 / 100;
+        double valorDesc = valorReais - desconto;
+
+        System.out.printf("\nValor do quilowatt: %.2f", valorKW);
+        System.out.printf("\nValor a ser pago: %.2f", valorReais);
+        System.out.printf("\nValor com desconto: %.2f\n", valorDesc);
+
+        scan.close();
+    }
     
 }
