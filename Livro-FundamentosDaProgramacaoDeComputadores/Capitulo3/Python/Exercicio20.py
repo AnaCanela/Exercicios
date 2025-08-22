8770fd5e7e9dd42bbd9949b35b2c14ad20453610
AnaCanela <anacanellag3@gmail.com>
2025-08-22 16:31:34 -0300

    Update Exercicio20.py

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/Python/Exercicio20.py
@@ -0,0 +1,14 @@
+#20. Faça um programa que receba a medida do ângulo (em graus) formado por uma escada apoiada no
+#chão e encostada na parede e a altura da parede onde está a ponta da escada. Calcule e mostre a medida dessa escada.
+#Observação: as funções trigonométricas implementadas nas linguagens de programação trabalham
+#com medidas de ângulos em radianos.
+
+import math
+
+angulo = float(input("Digite o angulo: "))
+altura = float(input("Digite a altura: "))
+
+radiano = (angulo * 3.14) / 180
+escada = altura / sin(radiano)
+
+print("A media da escada é ", escada)
