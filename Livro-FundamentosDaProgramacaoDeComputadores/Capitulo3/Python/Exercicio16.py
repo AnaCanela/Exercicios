af58415e26f93d7cd7f33bea359f07b832ccae9e
AnaCanela <anacanellag3@gmail.com>
2025-08-22 15:20:46 -0300

    Update Exercicio16.py

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo3/Python/Exercicio16.py
@@ -0,0 +1,16 @@
+#16. Faça um programa que receba o número de horas trabalhadas e o valor do salário mínimo,
+#calcule e mostre o salário a receber, seguindo estas regras:
+#    a) a hora trabalhada vale a metade do salário mínimo.
+#    b) o salário bruto equivale ao número de horas trabalhadas multiplicado pelo valor da hora trabalhada.
+#    c) o imposto equivale a 3% do salário bruto.
+#    d) o salário a receber equivale ao salário bruto menos o imposto.
+
+horasTrabalhadas = float(input("Digite a quantidade de horas trabalhadas: "))
+salario = float(input("Digite o valor do salário: "))
+
+valorHTrab = salario / 2
+salBruto = valorHTrab * horasTrabalhadas
+imposto = salBruto * 3 / 100
+salLiq = salBruto - imposto
+
+print("Salário a receber: R$ ", salLiq)
