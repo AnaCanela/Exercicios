65161001a9008a632ecf955c3ce41ae7f8677603
AnaCanela <anacanellag3@gmail.com>
2025-08-26 18:11:18 -0300

    Create Exercicio4.py

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo4/Python/Exercicio4.py
@@ -0,0 +1,17 @@
+#4. Faça um programa que receba três números e mostre-os em ordem crescente. Suponha que o usuário
+# digitará três números diferentes.
+
+num1, num2, num3 = map(float, input("Digite três números: ").split())
+
+if num1 < num2 < num3:
+    print(num1, "-", num2, "-", num3)
+elif num1 < num3 < num2:
+    print(num1, "-", num3, "-", num2)
+elif num2 < num1 < num3:
+    print(num2, "-", num1, "-", num3)
+elif num2 < num3 < num1:
+    print(num2, "-", num3, "-", num1)
+elif num3 < num1 < num2:
+    print(num3, "-", num1, "-", num2)
+elif num3 < num2 < num1:
+    print(num3, "-", num2, "-", num1)
