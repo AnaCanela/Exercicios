09ba47473213047165af99d7e1964333519f634a
AnaCanela <anacanellag3@gmail.com>
2025-08-27 13:30:39 -0300

    Create Exercicio6.py

---
+++ Livro-FundamentosDaProgramacaoDeComputadores/Capitulo4/Python/Exercicio6.py
@@ -0,0 +1,8 @@
+#6. Faça um programa que receba um número inteiro e verifique se é par ou ímpar.*/
+
+numero = float(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("\nÉ par!\n")
+else:
+    print("\nÉ ímpar!\n")
